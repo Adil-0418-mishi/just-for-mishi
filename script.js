d6460de492150045ec0218ea609f38a747d27877
Adil-0418-mishi <adimalik0122@gmail.com>
2026-06-28 11:24:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ function checkPassword() {
     const userPassword = document.querySelector("input").value;
 
     if (userPassword === correctPassword) {
-        alert("Welcome Mishi ❤️");
+
+        document.querySelector(".password-page").style.display = "none";
+        document.querySelector(".welcome-page").style.display = "flex";
+
     } else {
+
         alert("Oops! 🥺 Ye website sirf Mishi ke liye hai. ❤️");
+
     }
 }
 
 document.querySelector("button").addEventListener("click", checkPassword);
+
+document.getElementById("openBtn").addEventListener("click", function () {
+
+    alert("Next Surprise Coming Soon ❤️");
+
+});

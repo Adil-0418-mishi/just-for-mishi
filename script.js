bc3ce19464c792447d440da1d627e947a12e43f0
Adil-0418-mishi <adimalik0122@gmail.com>
2026-06-28 11:33:24 +0500

    Update script.js

--- script.js
+++ script.js
@@ -20,5 +20,40 @@ document.querySelector("button").addEventListener("click", checkPassword);
 document.getElementById("openBtn").addEventListener("click", function () {
 
     alert("Next Surprise Coming Soon ❤️");
+    document.getElementById("openBtn").addEventListener("click", function () {
+
+    document.querySelector(".welcome-page").style.display = "none";
+    document.querySelector(".intro-page").style.display = "flex";
+
+    typeWriter();
 
 });
+
+const text = `Hi Mishi... ❤️
+
+Ye sirf ek website nahi...
+
+Ye meri feelings hain.
+
+Har section me ek naya surprise tumhara intezar kar raha hai... 💖`;
+
+let i = 0;
+
+function typeWriter() {
+
+    if (i < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, 50);
+    }
+
+}
+
+document.getElementById("continueBtn").addEventListener("click", function () {
+
+    alert("Next Surprise ❤️");
+
+});
+
+});
+

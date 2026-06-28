1434221d81377374ad18ff89d9ae0da0ce659730
Adil-0418-mishi <adimalik0122@gmail.com>
2026-06-28 12:38:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -18,24 +18,26 @@ document.querySelector(".password-page button").addEventListener("click", functi
 
 });
 
-// Open It Button
+// Open Button
 document.getElementById("openBtn").addEventListener("click", function () {
 
     document.querySelector(".welcome-page").style.display = "none";
     document.querySelector(".intro-page").style.display = "flex";
 
+    document.getElementById("typing").innerHTML = "";
+    i = 0;
     typeWriter();
 
 });
 
-// Typewriter Text
+// Typewriter
 const text = `Hey Mishi ❤️
 
 Ye website sirf tumhare liye banayi hai...
 
 Har page par ek naya surprise tumhara intezar kar raha hai. ✨
 
-I hope tumhe ye pasand aaye. 💖`;
+I Hope Tumhe Ye Pasand Aaye. 💖`;
 
 let i = 0;
 
@@ -53,10 +55,72 @@ function typeWriter() {
 
 }
 
-// Continue Button
+// Continue
 document.getElementById("continueBtn").addEventListener("click", function () {
 
     document.querySelector(".intro-page").style.display = "none";
     document.querySelector(".love-page").style.display = "flex";
+
+});
+
+// Heart Page → Gallery
+document.querySelector(".heart").addEventListener("click", function () {
+
+    document.querySelector(".love-page").style.display = "none";
+    document.querySelector(".gallery-page").style.display = "flex";
+
+});
+
+// Gallery
+
+const photos = [
+
+"IMG-20260628-WA0013.jpg",
+"Snapchat-693309677.jpg",
+"IMG-20260606-WA0006.jpg",
+"IMG-20260602-WA0000.jpg"
+
+];
+
+const messages = [
+
+`💖 Photo 1
+
+Jab maine tumhe pehli baar dekha...
+Mujhe bilkul nahi pata tha ke tum meri zindagi ka sabse khoobsurat hissa ban jaogi. ❤️`,
+
+`🌸 Photo 2
+
+Tumhari muskurahat...
+Meri har udaasi ko khushi mein badal deti hai. 🥹❤️`,
+
+`🌹 Photo 3
+
+Har yaad jo tumhare saath bani...
+Meri zindagi ki sabse qeemti yaadon mein se ek hai. 🌸❤️`,
+
+`💞 Photo 4
+
+Chahe kitni bhi dooriyan aa jayein...
+Meri duaon mein tum hamesha rahogi. ❤️🫂`
+
+];
+
+let current = 0;
+
+document.getElementById("nextPhoto").addEventListener("click", function () {
+
+    current++;
+
+    if (current < photos.length) {
+
+        document.getElementById("photo").src = photos[current];
+        document.getElementById("photoText").innerText = messages[current];
+
+    } else {
+
+        alert("❤️ Thank You For Being In My Life ❤️");
+
+    }
 
 });
